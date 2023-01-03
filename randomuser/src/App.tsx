340645e66c3fb869ad53f4be5943752de9d2aebb
Farhana-Ahmed <farhana.khashiya@gmail.com>
2023-01-03 15:07:30 +0100

    yet to change the state of the name field

--- randomuser/src/App.tsx
+++ randomuser/src/App.tsx
@@ -18,12 +18,26 @@ fetch('https://randomuser.me/api/')
 .then((json) => setUser(json.results));
 }, [])
 
+const name = user?.map(item => item?.name.first);
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  e.preventDefault()
 
+  const value = e.currentTarget.value
+
+  // setUser({ ...user, name: value })
+}
   return (
     <div className="App">
       <form>
         <label>Name:</label>
+      {/* <input type="text" value={name} onChange={handleChange} /> */}
         <span>{user?.map(item => item?.name.first)}</span>
+        <hr></hr>
+        <label>Address:</label>
+        <span>{user?.map(item => item?.location.street.name)}</span>
+        <hr></hr>
+        <label>Age:</label>
+        <span>{user?.map(item => item?.dob.age)}</span>
         
       </form>
     </div>
