3c5a123bdb9c56d46905a5867272232a8acac00e
Farhana K <farhanak@Farhanas-Air.home>
2023-01-12 13:19:06 +0100

    fetch image

--- config/getImage.ts
+++ config/getImage.ts
@@ -3,7 +3,7 @@
 import { createApi } from "unsplash-js";
 
 const api = createApi({
-    accessKey: 'wjOvn2OYCht7qBr9ZL5fjrnHwG0E-u2s7K26CZ-4s4s'
+    accessKey: "process.env.ACCESS_KEY"
 })
 
 export const getImage = (query: any) => {
